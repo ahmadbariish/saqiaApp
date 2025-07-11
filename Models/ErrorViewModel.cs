333b2798657958800d1ac6bc66aa7ed4a6abf680
Ahmad Barish <ahmad.bariish@gmail.com>
2025-07-11 16:07:28 +0300

    create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SuqyaWaterSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
